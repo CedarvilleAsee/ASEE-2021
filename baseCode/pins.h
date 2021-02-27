071796e3d6269701fe48be856824db52284acb57
ASEECedarBot2 <CedarRobot2@gmail.com>
2021-02-27 11:37:49 -0500

    Update pins.h

--- baseCode/pins.h
+++ baseCode/pins.h
@@ -2,33 +2,33 @@
 #define PINS
 
 //display
-const int CS    = PC13;
-const int CLOCK = PC14;
-const int DIN   = PC15;
+//const int CS    = PC13;
+//const int CLOCK = PC14;
+//const int DIN   = PC15;
 
 // Buttons
-const int BUTTON_2 = PB4;//Go red button
-const int BUTTON_1 = PB5;//Go blue button
+const int BUTTON_2 = A0;//Go red button
+const int BUTTON_1 = A1;//Go blue button
 
 // Wheels
-const int WHEEL_DIR_LB = PB14;//BIN1
-const int WHEEL_DIR_LF = PB15;//BIN2
-const int WHEEL_DIR_RB = PB13;//AIN1
-const int WHEEL_DIR_RF = PB12;//AIN2
+const int WHEEL_DIR_LB = 26;//BIN1
+const int WHEEL_DIR_LF = 28;//BIN2
+const int WHEEL_DIR_RB = 36;//AIN1
+const int WHEEL_DIR_RF = 34;//AIN2
 
-const int WHEEL_SPEED_L = PB1; //PWMB
-const int WHEEL_SPEED_R = PB0; //PWMA
+const int WHEEL_SPEED_L = 7; //PWMB
+const int WHEEL_SPEED_R = 8; //PWMA
 
 // Line sensors
 //left to right is 7 to 0
-const int LINE_SENSOR[] = {PA4, PA5, PA6, PA7, PB3, PA15, PB10, PB11};
+const int LINE_SENSOR[] = {51, 49, 47, 45, 43, 41, 39, 37};
 
 //Servos
-const int LINE_SENSOR_SERVO = PB10;
-const int LAUNCHER_SERVO    = PB11;
+const int LINE_SENSOR_SERVO = 13;
+const int LAUNCHER_SERVO    = 12;
 
 //Other sensors
-const int DISTANCE_SENSOR   = PB9;
+//const int DISTANCE_SENSOR   = PB9;
 
 
 #endif
